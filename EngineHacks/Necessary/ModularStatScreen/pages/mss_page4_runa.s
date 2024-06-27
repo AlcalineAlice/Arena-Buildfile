4f485f8bea4c30dca53e39088edacd0e71ac8524
Alice <raulapablaza2001@gmail.com>
2024-06-26 22:45:27 -0400

    Stat Screen Fixes

--- EngineHacks/Necessary/ModularStatScreen/pages/mss_page4_runa.s
+++ EngineHacks/Necessary/ModularStatScreen/pages/mss_page4_runa.s
@@ -1,12 +1,6 @@
 .thumb
 .include "mss_defs.s"
 
-.global MSS_page4
-.type MSS_page4, %function
-
-
-MSS_page4:
-
 page_start
 
 draw_textID_at 13, 3, textID=0xd4c, width=16, colour=Blue
@@ -15,20 +9,19 @@ draw_textID_at 13, 3, textID=0xd4c, width=16, colour=Blue
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 @add    r1,#2
-add	   r0,r1
-ldrh   r0,[r0]		@load textid
+ldrh   r0,[r0,r1]		@load textid
 mov    r3, r7
 mov r1, #12
 ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -36,20 +29,15 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*3)+(2*17))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
-
-b LiteralJump1
-.ltorg
-.align
-LiteralJump1:
 
 @ second like
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 add    r1,#2
@@ -60,7 +48,7 @@ ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -68,23 +56,17 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*5)+(2*17))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
 
 draw_textID_at 13, 7, textID=0xd4d, width=16, colour=Blue
-
-b LiteralJump2
-.ltorg
-.align
-LiteralJump2:
-
 
 @ first dislike
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 add    r1,#4
@@ -95,7 +77,7 @@ ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -103,15 +85,15 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*7)+(2*17))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
 
 @ second dislike
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 add    r1,#6
@@ -122,7 +104,7 @@ ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -130,23 +112,17 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*9)+(2*17))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
 
 draw_textID_at 13, 11, textID=0xd4f, width=16, colour=Blue
-
-b LiteralJump3
-.ltorg
-.align
-LiteralJump3:
-
 
 @ age
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 add    r1,#8
@@ -157,7 +133,7 @@ ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -165,23 +141,17 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*11)+(2*16))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
 
 draw_textID_at 21, 11, textID=0xd4e, width=16, colour=Blue
-
-b LiteralJump4
-.ltorg
-.align
-LiteralJump4:
-
 
 @ height
 mov    r0,r8
 ldr    r1,[r0]               @load character pointer
 ldrb   r1,[r1,#0x4]	         @load character number
-ldr    r0,=PersonalDataTable  @load first like
-@ldr    r0,[r0]
+adr    r0,PersonalInfoTable  @load first like
+ldr    r0,[r0]
 mov    r2,#12
 mul    r1,r2
 add    r1,#10
@@ -192,7 +162,7 @@ ldrh r2,[r3] @current number
 add r2,r1 @for the next one.
 strb r1, [r3, #4] @store width
 strb r2, [r3, #8] @assign the next one.
-blh String_GetFromIndex
+blh BufferText
 mov    r2, #0x0
 str    r2, [sp]
 str    r0, [sp, #4]
@@ -200,13 +170,11 @@ mov    r2, #0 @colour
 mov    r0, r7
 ldr    r1, =(tile_origin+(0x20*2*11)+(2*25))
 mov    r3, #0
-blh    DrawTextInline, r4
+blh    DrawText, r4
 add    r7, #8
-
-@ Next let's draw Gaiden spells if the hack is installed.
-draw_gaiden_spells_at 13, 13, GaidenStatScreen @ GaidenStatScreen is a pointer to the routine, GaidenStatScreen.
 
 page_end
 
 .align
 .ltorg
+PersonalInfoTable:
